bbcd3f45d4e4d8e3c029032adfb4c483a707efb3
Patrick <porchewellness@gmail.com>
2017-07-31 11:25:03 -0700

    views for login and signup

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,12 +2,35 @@ const router = require('express').Router();
 const contacts = require('./contacts')
 const DbContacts = require('../../db/contacts');
 
-router.get('/', (request, response) => {
-  DbContacts.getContacts()
-    .then((contacts) => {response.render('index', { contacts })})
-    .catch( err => console.log('err', err) )
+
+router.get('/signup', (request, response) => {
+	response.render('signup')
 })
 
+router.post('/signup', (request, response) => {
+
+	response.redirect('login')
+})
+
+router.get('/login', (request, response) => {
+	response.render('login')
+})
+
+// PASSPORT STUFF
+
+router.post('/login', (request, response) => {
+
+	// response.redirect()
+})
+
+router.get('/', (request, response) => {
+	DbContacts.getContacts()
+	.then((contacts) => {response.render('index', { contacts })})
+	.catch( err => console.log('err', err) )
+})
+
+
 router.use('/contacts', contacts); // /contacts/search
+// router.use('/auth', auth) // /auth/signup ...
 
 module.exports = router;
