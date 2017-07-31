75a5c960bb70f72eba53a7e88ca8a743c9aa5c45
Patrick <porchewellness@gmail.com>
2017-07-31 13:27:13 -0700

    updated form on new contact page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -16,19 +16,18 @@ router.get('/login', (request, response) => {
 	response.render('login')
 })
 
-// PASSPORT STUFF
-
 router.post('/login', (request, response) => {
 
 	// response.redirect()
 })
+
+// PASSPORT STUFF
 
 router.get('/', (request, response) => {
 	DbContacts.getContacts()
 	.then((contacts) => {response.render('index', { contacts })})
 	.catch( err => console.log('err', err) )
 })
-
 
 router.use('/contacts', contacts); // /contacts/search
 // router.use('/auth', auth) // /auth/signup ...
