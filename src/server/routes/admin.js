8040f2b58558983d83c585a6f15c759ad5f77149
Patrick <porchewellness@gmail.com>
2017-08-03 14:35:03 -0700

    updates file structure in views

--- src/server/routes/admin.js
+++ src/server/routes/admin.js
@@ -3,15 +3,15 @@ const {renderError} = require('../utils')
 const router = require('express').Router()
 
 router.get('/new', (request, response) => {
-  if (response.locals.isAdmin === true) {
+  if (response.locals.isAdmin) {
     response.render('new')
   } else {
-    response.status('403').render('not_authorized')
+    response.status('403').render('errors/not_authorized')
   }
 })
 
 router.post('/', (request, response, next) => {
-  if (response.locals.isAdmin === true) {
+  if (response.locals.isAdmin) {
     DbContacts.createContact(request.body)
       .then(function(contact) {
         if (contact) return response.redirect(`/contacts/${contact[0].id}`)
@@ -19,13 +19,13 @@ router.post('/', (request, response, next) => {
       })
       .catch( error => renderError(error, response, response) )
   } else {
-    response.status('403').render('not_authorized')
+    response.status('403').render('errors/not_authorized')
   }
 })
 
 router.get('/:contactId/delete', (request, response, next) => {
   const contactId = request.params.contactId
-  if (response.locals.isAdmin === true) {
+  if (response.locals.isAdmin) {
     DbContacts.deleteContact(contactId)
       .then(function(contact) {
         if (contact) return response.redirect('/')
@@ -33,7 +33,7 @@ router.get('/:contactId/delete', (request, response, next) => {
       })
       .catch( error => renderError(error, response, response) )
   } else {
-    response.status('403').render('not_authorized')
+    response.status('403').render('errors/not_authorized')
   }
 })
 
